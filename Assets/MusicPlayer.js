c2f9ffc5f7f317202d4c77a02ffbed22c82e94df
Keijiro Takahashi <keijiro@gmail.com>
2012-10-01 14:56:20 +0900

    streaming experiment

--- Assets/MusicPlayer.js
+++ Assets/MusicPlayer.js
@@ -1,10 +1,17 @@
 #pragma strict
 
 function Start() {
-	yield WaitForSeconds(1.0);
-	
 	var audios = GetComponents.<AudioSource>();
-	
+
+	audios[0].clip = WWW("http://cloud.github.com/downloads/keijiro/unity-musicloop-example/Intro.ogg").GetAudioClip(false, true, AudioType.OGGVORBIS);
+	audios[1].clip = WWW("http://cloud.github.com/downloads/keijiro/unity-musicloop-example/Loop.ogg").GetAudioClip(false, true, AudioType.OGGVORBIS);
+
+	Debug.Log("Loading...");
+
+	while (!audios[0].clip.isReadyToPlay || !audios[1].clip.isReadyToPlay) yield;
+
+	Debug.Log("Play");
+
 	audios[0].Play();
 	audios[1].Play(audios[0].clip.samples);
 }
